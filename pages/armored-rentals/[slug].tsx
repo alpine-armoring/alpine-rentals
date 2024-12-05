410846838e512cf028867d29089b1cb7ba40b080
Stefan <petkovics93@yahoo.com>
2024-12-05 14:49:49 +0100

    stock new description

--- pages/armored-rentals/[slug].tsx
+++ pages/armored-rentals/[slug].tsx
@@ -2,11 +2,8 @@ import styles from './InventoryVehicle.module.scss';
 import { getPageData } from 'hooks/api';
 import { useState, useEffect } from 'react';
 import dynamic from 'next/dynamic';
-const DownloadIcon = dynamic(() => import('components/icons/Download'));
 const InfoIcon = dynamic(() => import('components/icons/Info'));
-const PDFIcon = dynamic(() => import('components/icons/PDF2'));
 import Link from 'next/link';
-// import StickyHorizontalSlider from 'components/global/sticky-horizontal-slider/StickyHorizontalSlider';
 import Button from 'components/global/button/Button';
 import Carousel from 'components/global/carousel/Carousel';
 import LightboxCustom from 'components/global/lightbox/LightboxCustom';
@@ -21,23 +18,15 @@ function InventoryVehicle(props) {
   const data =
     props && props.data && props.data.data[0] && props.data.data[0].attributes;
   const topGallery = data?.rentalsGallery?.data;
-  const mainText = data?.description;
-  // const category = data?.categories?.data[0]?.attributes?.title;
-  // const categorySlug = data?.categories?.data[0]?.attributes?.slug;
-
+  const mainText = data?.rentalsDescription;
   const videoWebm = data?.video?.data?.attributes;
   const videoMP4 = data?.videoMP4?.data?.attributes;
 
   const convertMarkdown = useMarkdownToHtml();
 
-  // const [thumbsAxis, setThumbsAxis] = useState<'x' | 'y'>('x');
-  // useEffect(() => {
-  //   window.innerWidth >= 1600 ? setThumbsAxis('y') : setThumbsAxis('x');
-  // }, []);
   const sliderTopOptions = {
     dragFree: false,
     loop: true,
-    // axis: thumbsAxis,
     thumbs: true,
   };
 
@@ -141,7 +130,9 @@ function InventoryVehicle(props) {
 
           <div className={`${styles.inventory_heading_title}`}>
             {data?.title ? (
-              <h1 dangerouslySetInnerHTML={{ __html: data.title }}></h1>
+              <h1
+                dangerouslySetInnerHTML={{ __html: `Rental ${data.title}` }}
+              ></h1>
             ) : null}
           </div>
 
@@ -214,17 +205,6 @@ function InventoryVehicle(props) {
               ) : null}
             </div>
 
-            <div className={`${styles.inventory_cta_wrap}`}>
-              <Button
-                onClick={scroll}
-                button={true}
-                className={`${styles.inventory_cta} primary attention`}
-                attention
-              >
-                Request a quote
-              </Button>
-            </div>
-
             <div
               className={`${styles.inventory_top_shape} shape-before shape-before-dark mobile-only`}
             ></div>
@@ -284,37 +264,20 @@ function InventoryVehicle(props) {
               })}
             </ul>
 
-            {data?.OEMWindowSticker?.data || data?.OEMArmoringSpecs?.data ? (
-              <div className={`${styles.inventory_pdfs}`}>
-                {data?.OEMWindowSticker?.data ? (
-                  <Button
-                    href={data.OEMWindowSticker.data.attributes?.url.replace(
-                      /\.ai$/,
-                      '.pdf'
-                    )}
-                    iconComponent={PDFIcon}
-                    className={`${styles.inventory_pdfs_button} icon rounded`}
-                    target
-                  >
-                    <strong>OEM</strong> Window Sticker
-                  </Button>
-                ) : null}
+            <p className={`${styles.inventory_details_description}`}>
+              Renting armored [SUV]
+            </p>
 
-                {data?.OEMArmoringSpecs?.data ? (
-                  <Button
-                    href={data.OEMArmoringSpecs.data.attributes?.url.replace(
-                      /\.ai$/,
-                      '.pdf'
-                    )}
-                    iconComponent={DownloadIcon}
-                    className={`${styles.inventory_pdfs_button} icon rounded`}
-                    target
-                  >
-                    Armoring Specs
-                  </Button>
-                ) : null}
-              </div>
-            ) : null}
+            <div className={`${styles.inventory_cta_wrap}`}>
+              <Button
+                onClick={scroll}
+                button={true}
+                className={`${styles.inventory_cta} primary attention`}
+                attention
+              >
+                Request a quote
+              </Button>
+            </div>
           </div>
         </div>
       </div>
@@ -325,26 +288,6 @@ function InventoryVehicle(props) {
           dangerouslySetInnerHTML={{ __html: convertMarkdown(mainText) }}
         ></div>
       ) : null}
-
-      {/* {data?.specifications?.data.length > 0 ? (
-        <div id="armoring-specs" className={`${styles.inventory_specs} anchor`}>
-          <StickyHorizontalSlider
-            slides={data.specifications.data}
-            title="Armoring Specifications"
-            inventory
-          />
-        </div>
-      ) : null} */}
-
-      {/* {data?.accessories?.data.length > 0 ? (
-        <div id="options-included" className={`anchor`}>
-          <StickyHorizontalSlider
-            slides={data.accessories.data}
-            title="Options Included"
-            inventory
-          />
-        </div>
-      ) : null} */}
 
       {videoWebm || videoMP4 ? (
         <VideoScale videoWebm={videoWebm} videoMP4={videoMP4} />
