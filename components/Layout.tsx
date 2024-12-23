efee8fba354854919d773e2350638de7df6e5a4f
Stefan <petkovics93@yahoo.com>
2024-12-23 18:14:37 +0100

    GA

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import localFont from 'next/font/local';
 import dynamic from 'next/dynamic';
-// import Script from 'next/script';
+import Script from 'next/script';
 
 import Header from './global/header/Header';
 import Footer from './global/footer/Footer';
@@ -48,31 +48,11 @@ const termina = localFont({
 const Layout = ({ children }) => {
   const router = useRouter();
 
-  const pathsDarkMode = [
-    '/available-now',
-    '/rental-vehicles',
-    '/manufacturing',
-    '/sold-vehicles',
-    '/ballistic-testing',
-  ];
-  const pathsDarkFooter = ['/manufacturing', '/ballistic-testing'];
-  const pathsPadding0 = ['/news/', '/ballistic-testing'];
-
-  const isDarkMode = pathsDarkMode.some((path) =>
-    router.pathname.startsWith(path)
-  );
-  const isDarkFooter = pathsDarkFooter.some((path) =>
-    router.pathname.startsWith(path)
-  );
   const isHomepage = router.pathname === '/';
 
   const pathsHeaderTransparent = ['/ballistic-testing'];
   const isHeaderGray = pathsHeaderTransparent.some(
     (path) => router.pathname.startsWith(path) || isHomepage
-  );
-
-  const isPadding0 = pathsPadding0.some((path) =>
-    router.pathname.startsWith(path)
   );
 
   const [isNavOpen, setNavOpen] = useState(false);
@@ -81,34 +61,6 @@ const Layout = ({ children }) => {
     <>
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        {isDarkMode && (
-          <style>{`
-              body {
-                background: #101010 url(/assets/noise4.png) !important;
-                background-size: 30px !important;
-              }
-              .navigation_submenu{
-                background-color: rgba(23, 23, 23, 0.8) !important;
-                backdrop-filter: blur(10px) !important;
-              }
-              .navigation_submenu a{
-                color: white;
-              }
-              .header_logo_gold{
-                display: block !important;
-              }
-              .header_logo_black{
-                display: none;
-              }
-            `}</style>
-        )}
-        {(isPadding0 || isHomepage) && (
-          <style>{`
-              body {
-                padding-top: 0 !important;
-              }
-            `}</style>
-        )}
         {isHeaderGray && (
           <style>{`
               header,
@@ -130,8 +82,8 @@ const Layout = ({ children }) => {
       </Head>
 
       {/* Google Tag Manager */}
-      {/* <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-HQE6WWZ9E4"
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-MXGGKXK12K"
         strategy="afterInteractive"
       />
       <Script
@@ -142,14 +94,13 @@ const Layout = ({ children }) => {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-HQE6WWZ9E4', { page_path: window.location.pathname });
+            gtag('config', 'G-MXGGKXK12K', { page_path: window.location.pathname });
           `,
         }}
-      /> */}
+      />
 
       <div className={termina.className}>
         <Header
-          isDarkMode={isDarkMode}
           setNavOpen={setNavOpen}
           isNavOpen={isNavOpen}
           isHeaderGray={isHeaderGray}
@@ -161,7 +112,7 @@ const Layout = ({ children }) => {
 
         <ScrollToTopButton />
 
-        <Footer isDarkMode={isDarkFooter} />
+        <Footer />
       </div>
     </>
   );
